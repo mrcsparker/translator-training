988cc074254496349762f226d760346853eaa561
Chris Parker <mrcsparker@gmail.com>
2015-10-28 22:37:54 -0500

    Initial setup of json server

---
+++ jsonserve/src/main/java/jsonserve/JsonServeConfiguration.java
@@ -0,0 +1,13 @@
+package jsonserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonServeConfiguration {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsonServeConfiguration.class, args);
+    }
+
+}
