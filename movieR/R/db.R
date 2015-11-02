5643854346eaebd8942120c06a1060d3cd3860ed
Chris Parker <mrcsparker@gmail.com>
2015-11-01 19:27:22 -0600

    Added dev versions of some packages

--- movieR/R/db.R
+++ movieR/R/db.R
@@ -1,11 +1,6 @@
-# Hello, world!
-#
-# This is an example function named 'hello'
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
+# Need prepared statements support:
+#   - devtools::install_github("rstats-db/DBI")
+#   - devtools::install_github("rstats-db/RMySQL")
 #
 # Some useful keyboard shortcuts for package authoring:
 #
@@ -14,10 +9,18 @@
 #   Test Package:              'Cmd + Shift + T'
 
 library(R6)
+library(DBI)
 library(RMySQL)
 library(dplyr)
 library(jsonlite)
 
+#' DB Class
+#' #'
+#' @docType class
+#' @importFrom R6 R6Class
+#' @export
+#' @format An \code{\link{R6Class}} generator object
+#' @keywords data
 DB <- R6Class("R6",
   public = list(
     conn = NA,
@@ -31,7 +34,7 @@ DB <- R6Class("R6",
     },
 
     moviesByYear = function() {
-      self$conn %>% tbl("movies") %>% filter(year > 2010) %>% group_by(year) %>% summarize(n())
+      self$conn %>% tbl("movies") %>% filter(year > 2010) %>% group_by(year) %>% summarize(count = n())
     },
 
     moviesWithRatings = function() {
@@ -41,8 +44,42 @@ DB <- R6Class("R6",
       movies %>% inner_join(ratings, by = c("id" = "movie_id"))
     },
 
+    insertChart = function(chartName, chartType, chartJson) {
+      self$conn$con %>%
+        dbSendQuery(paste("INSERT INTO services ( ",
+                          "name, chart_type, json ",
+                          ") VALUES ( ",
+                          ":name, :chartType, :json ",
+                          ")"),
+                    params = list(name = chartName,
+                                  chartType = chartType,
+                                  json = chartJson))
+    },
+
+    updateChart = function(chartName, chartType, chartJson) {
+      self$conn$con %>%
+        dbSendQuery(paste("UPDATE services ",
+                          "SET name = :name, ",
+                          "chart_type = :chartType, ",
+                          "json = :json ",
+                          "WHERE name = :name AND chart_type = :chartType"),
+                    params = list(name = chartName,
+                                  chartType = chartType,
+                                  json = chartJson))
+    },
+
     saveChart = function(chartName, chartType, chartJson) {
-      service <- self$conn %>% tbl("services") %>% filter(name = chartName, type = chartType)
+      services <- self$conn %>% tbl("services") %>%
+        filter(name == chartName, chart_type == chartType) %>%
+        summarize(count = n()) %>%
+        collect()
+
+      if (services$count == 0) {
+        self$insertChart(chartName, chartType, chartJson)
+      } else {
+        self$updateChart(chartName, chartType, chartJson)
+      }
+
     }
   )
 )
