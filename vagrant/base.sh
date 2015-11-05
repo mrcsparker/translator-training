45e77dce730ec63f97c56533c055b99913793702
Chris Parker <mrcsparker@gmail.com>
2015-11-05 13:28:36 -0600

    Updated base and ruby

--- vagrant/base.sh
+++ vagrant/base.sh
@@ -14,5 +14,23 @@ if [ ! -f /opt/provision/base ]; then
   apt-get install -y libcurl4-gnutls-dev
   apt-get install -y git
 
+  apt-get install -y git-core
+  apt-get install -y curl
+  apt-get install -y zlib1g-dev
+  apt-get install -y build-essential
+  apt-get install -y libssl-dev
+  apt-get install -y libreadline-dev
+  apt-get install -y libyaml-dev
+  apt-get install -y libsqlite3-dev
+  apt-get install -y sqlite3
+  apt-get install -y libxml2-dev
+  apt-get install -y libxslt1-dev
+  apt-get install -y libcurl4-openssl-dev
+  apt-get install -y python-software-properties
+  apt-get install -y libffi-dev
+  apt-get install -y libgdbm-dev
+  apt-get install -y libtool
+  apt-get install -y bison
+
   touch /opt/provision/base
 fi
